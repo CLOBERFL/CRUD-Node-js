2329c41c20f755f171f8712cd09e34f24a7f680e
Camilo <camiloxlozano@gmail.com>
2019-08-12 21:39:10 +0200

    Funcion eliminar los resultados de la lista

--- src/routes/index.js
+++ src/routes/index.js
@@ -14,6 +14,13 @@ router.post('/add', async (req , res) =>{
     const task = new Task(req.body);
     await task.save();
     res.redirect('/');
-})
+});
+
+router.get('/delete/:id', async (req, res) =>{
+
+    const {id} = req.params;
+    await Task.remove({_id : id});
+    res.redirect('/');
+});
 
 module.exports = router;
