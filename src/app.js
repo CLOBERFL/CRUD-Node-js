1a0caee8405fc953582163928034e37fa52c5eb2
Camilo <camiloxlozano@gmail.com>
2019-08-12 15:19:41 +0200

    Creacion del servidor y de la Bd.

---
+++ src/app.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const app = express();
+
+//Conectando la base de datos
+
+mongoose.connect('mongodb://localhost/crud-mongo')
+    .then(db => console.log ('Db Connected'))
+    .catch(err => console.log(err));
+
+//Importing routes
+const indexRoutes = require('./routes/index');
+
+//Settings
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname + '/views'));
+app.set('view engine', 'ejs');
+
+//Middlewares (Procesar los datos antes de que lleguen a las rutas)
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended:false}));
+
+//Routes
+app.use('/', indexRoutes);
+
+//Startind the server
+app.listen(app.get('port'), () => {
+    console.log(`Server on port ${app.get('port')}`)
+});
