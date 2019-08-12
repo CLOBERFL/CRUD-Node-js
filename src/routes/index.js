006e9b37a90a6d1d094fa02c6c85e1f1ae8de784
Camilo <camiloxlozano@gmail.com>
2019-08-12 16:40:07 +0200

    Objeto almacenado en la Base de datos de mongo

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
+const Task = require('../models/task');
+
+router.get('/' , (req, res) =>{
+    res.render('index');
+});
+router.post('/add', async (req , res) =>{
+    const task = new Task(req.body);
+    await task.save();
+    res.send('recibido');
+})
 
 module.exports = router;
